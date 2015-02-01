0ed7db7e911650d2708407578720a7b3d62476aa
Greg Sterndale <gsterndale@gmail.com>
2015-02-01 14:09:49 -0500

    Fix github URL

--- git_refactor_replace_formula.rb
+++ git_refactor_replace_formula.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class GitRefactorReplaceFormula < Formula
   homepage 'https://github.com/promptworks/git-refactor-replace'
-  head 'https://github.com/promptworks/git-refactor-rename.git'
+  head 'https://github.com/promptworks/git-refactor-replace.git'
 
   def install
     bin.install 'git-refactor-replace', 'git-refactor-rename'
